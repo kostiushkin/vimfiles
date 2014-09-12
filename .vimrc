7f76f15aefe057a1411193817c33d2299f6ee5d1
Sergiy Kostyushkin <s.kostyushkin@gmail.com>
2014-09-12 10:46:23 +0200

    Initial commit

---
+++ .vimrc
@@ -0,0 +1,10 @@
+colorscheme railscasts
+call pathogen#infect()
+call pathogen#helptags()
+set softtabstop=4
+set shiftwidth=4
+set tabstop=4
+set expandtab
+set nu
+imap <S-F12> <S-Insert>
+map nt :NERDTreeTabsToggle<Enter>
---
+++ deploy_vim_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get install curl
+mkdir -p ~/.vim/colors ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vimrc https://raw.githubusercontent.com/kostyushkin/vimfiles/master/.vimrc
+curl -LSso ~/.vim/colors/railscasts.vim https://raw.githubusercontent.com/jpo/vim-railscasts-theme/master/colors/railscasts.vim
+curl -LSso ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+git clone https://github.com/jimenezrick/vimerl ~/.vim/bundle/vimerl
+git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp.vim
+git clone https://github.com/scrooloose/nerdtree ~/.vim/bundle/nerdtree
+git clone https://github.com/jistr/vim-nerdtree-tabs ~/.vim/bundle/vim-nerdtree-tabs
